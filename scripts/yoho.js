11bd8311f45890e6e6198cfa13d4d09665bd3e30
EmoSquidling <129456278+EmoSquidling@users.noreply.github.com>
2024-12-20 23:14:02 -0600

    Update yoho.js

--- scripts/yoho.js
+++ scripts/yoho.js
@@ -292,7 +292,8 @@ function ahoyMaties() {
     if (getProperty("choiceAdventure1540") != 2) cliExecute("set choiceAdventure1540 = 2");
     if (getProperty("choiceAdventure1541") != 2) cliExecute("set choiceAdventure1541 = 2");
     if (getProperty("choiceAdventure1542") != 2) cliExecute("set choiceAdventure1542 = 2");
-};
+
+}
 
 /**
  * Execute sources for buffs up to a given # of turns.
