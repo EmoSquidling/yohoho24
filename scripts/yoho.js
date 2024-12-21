69739367e42cf72cedee16e0124389c9c8fcdaee
EmoSquidling <129456278+EmoSquidling@users.noreply.github.com>
2024-12-20 23:20:22 -0600

    Update yoho.js

--- scripts/yoho.js
+++ scripts/yoho.js
@@ -244,7 +244,7 @@ function ahoyMaties() {
     //   accessory is +5 res vs -1 combat
    // if (getProperty("horseryAvailable") === "true") {
        // if (getProperty("_horsery") != "dark horse") cliExecute("horsery dark horse");
-    }
+    
 
     // Grab a fish hatchet from the floundry.
     if (getProperty("_floundryItemCreated") === "false") {
@@ -292,7 +292,7 @@ function ahoyMaties() {
     if (getProperty("choiceAdventure1540") != 2) cliExecute("set choiceAdventure1540 = 2");
     if (getProperty("choiceAdventure1541") != 2) cliExecute("set choiceAdventure1541 = 2");
 
-
+}
 
 /**
  * Execute sources for buffs up to a given # of turns.
