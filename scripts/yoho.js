ca91fce1fc9fc04a5ddf26f19b62d49bf65837d0
EmoSquidling <129456278+EmoSquidling@users.noreply.github.com>
2024-12-25 20:02:59 -0600

    Update yoho.js

--- scripts/yoho.js
+++ scripts/yoho.js
@@ -409,7 +409,7 @@ function manageEquipment(island) {
     checkThenEquip("pants",toItem("lava-proof pants"));
     checkThenEquip("acc1",toItem("mafia thumb ring"));
     checkThenEquip("acc2",toItem("mime army infiltration glove"));
-    checkThenEquip("acc3",toItem("duonoculars"));
+    checkThenEquip("acc3",toItem("red shoe"));
     
     //if (numericModifier(toElement(ISLANDRESMAP[island])+" resistance") < 40) {
         //checkThenEquip("acc3",toItem("Pocket Square of Loathing"));
